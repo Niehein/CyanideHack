522faa20c68d0f83d095e38716a00c791fa19f56
FloGo3308 <flogo3308@gmail.com>
2021-05-18 20:00:00 +0200

    Added a basic module list with R A I N B O W

--- src/main/java/me/niehein/cyanidehack/gui/HUD.java
+++ src/main/java/me/niehein/cyanidehack/gui/HUD.java
@@ -1,15 +1,35 @@
 package me.niehein.cyanidehack.gui;
 
+import me.niehein.cyanidehack.CyanideHack;
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.gui.FontRenderer;
 import net.minecraft.client.gui.Gui;
 import net.minecraft.client.gui.ScaledResolution;
+import net.minecraft.client.renderer.GlStateManager;
 
 public class HUD extends Gui {
     private Minecraft mc = Minecraft.getMinecraft();
 
+    int offset = 2;
+    String[] list0 = {
+            "a",
+            "b",
+            "c",
+            "d",
+            "e"
+    };
+
     public void draw() {
         ScaledResolution sr = new ScaledResolution(mc);
         FontRenderer fr = mc.fontRenderer;
+        int count = 0;
+
+        GlStateManager.scale(2, 2, 1);
+        for (String str : list0) {
+            fr.drawStringWithShadow(str, offset, offset + (fr.FONT_HEIGHT+offset) * count, CyanideHack.getRainbow(1, count*10).getRGB());
+            count++;
+        }
+        GlStateManager.scale(0.5, 0.5, 1);
+        GlStateManager.color(1.0F, 1.0F, 1.0F);
     }
 }
